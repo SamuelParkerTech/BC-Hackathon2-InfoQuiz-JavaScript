be5f126000b171067324a6246cb2ef5a0d4dfd2a
Tanika Irwin <tanika-i@outlook.com>
2024-07-03 11:58:54 +0000

    adding next question button on right end of quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
 const API_URL = "https://opentdb.com/api.php?amount=20&type=multiple";
 
-// Event listeners for buttons that restart the quiz, submits your answer and goes to next question
+document.addEventListener("DOMContentLoaded", function() {
+
+const _question = document.getElementById('question');
+
+// Event listeners for buttons that restarts the quiz, submits your answer and goes to next question
 document.getElementById("restart-quiz").addEventListener("click", e => restartsQuiz(e));
 document.getElementById("submit-answer").addEventListener("click", e => submitsAnswer(e));
 document.getElementById("next-question").addEventListener("click", e => getNextQuestion(e));
 
-// async function getQuestion(e) {
+// function displayQuestion(data) {
+
+    
+//     document.getElementById("question").innerHTML = result;
+//     _question.show();
+// }
+
+// async function restartsQuiz(e) {
 //     const result = await fetch(`${API_URL}`)
 //     const data = await result.json();
-//     result.innerHTML = "";
-//     showQuestion(data.results[0]);
-// }

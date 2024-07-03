688f04f3337b3255e3fcde1d3086a2f8a401809c
Tanika Irwin <tanika-i@outlook.com>
2024-07-03 10:45:32 +0000

    adding event listeners for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 const API_URL = "https://opentdb.com/api.php?amount=20&type=multiple";
+
+// Event listeners for buttons that restart the quiz, submits your answer and goes to next question
+document.getElementById("restart-quiz").addEventListener("click", e => restartsQuiz(e));
+document.getElementById("submit-answer").addEventListener("click", e => submitsAnswer(e));
+document.getElementById("next-question").addEventListener("click", e => getNextQuestion(e));
+
+// async function getQuestion(e) {
+//     const result = await fetch(`${API_URL}`)
+//     const data = await result.json();
+//     result.innerHTML = "";
+//     showQuestion(data.results[0]);
+// }
